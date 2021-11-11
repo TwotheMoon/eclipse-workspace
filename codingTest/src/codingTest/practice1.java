33e48e7577871d792500414e59d853031893c027
2theMoon <dlaguddh1@gmail.com>
2021-11-11 11:46:41 +0900

    기본 코테 연습

---
+++ codingTest/src/codingTest/practice1.java
@@ -0,0 +1,32 @@
+package codingTest;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+
+		// N 까지의 수 합
+		int result = 0;
+		int n = 100;
+//		for(int i = 1; i <= n; i++) {
+//			result += i;
+//		}
+		
+		result = n * (n + 1) / 2;
+		System.out.println(result);
+		
+		
+		
+		// for문 구구단
+		for(int i = 1; i <= 9; i++) {
+			System.out.println("<" + i + "단 >");
+			
+			for(int j = 1; j <= 9; j++) {
+				System.out.println(i + "X" + j + "=" + i * j );
+			}
+		} 
+		
+		// 로또 번호 추출
+		
+	}
+	
+}
