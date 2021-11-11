b0aaf3f58451276cc08bf66749166dc559b34bc6
2theMoon <dlaguddh1@gmail.com>
2021-11-11 16:24:54 +0900

    백준 15552

--- codingTest/src/codingTest/practice1.java
+++ codingTest/src/codingTest/practice1.java
@@ -10,22 +10,36 @@ public class practice1 {
 //		for(int i = 1; i <= n; i++) {
 //			result += i;
 //		}
+//		
+//		result = n * (n + 1) / 2;
+//		System.out.println(result);
+//		
+//		
+//		
+//		// for문 구구단
+//		for(int i = 1; i <= 9; i++) {
+//			System.out.println("<" + i + "단 >");
+//			
+//			for(int j = 1; j <= 9; j++) {
+//				System.out.println(i + "X" + j + "=" + i * j );
+//			}
+//		} 
 		
-		result = n * (n + 1) / 2;
-		System.out.println(result);
-		
-		
-		
-		// for문 구구단
-		for(int i = 1; i <= 9; i++) {
-			System.out.println("<" + i + "단 >");
-			
-			for(int j = 1; j <= 9; j++) {
-				System.out.println(i + "X" + j + "=" + i * j );
-			}
-		} 
-		
-		// 로또 번호 추출
+		int a = 7;
+        int b = 3;
+        
+        int add = a + b;
+        int minus = a - b;
+        int mult = a * b;
+        int div = a / b;
+        int mod = a % b;
+       
+	  System.out.println(add);
+	  System.out.println(minus);
+	  System.out.println(mult);
+	  System.out.println(div);
+	  System.out.println(mod);
+	  
 		
 	}
 	
