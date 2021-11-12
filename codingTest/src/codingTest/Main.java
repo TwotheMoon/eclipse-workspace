fe48995c95fbb9c476da27f14056853f4fd25293
2theMoon <dlaguddh1@gmail.com>
2021-11-12 12:11:16 +0900

    8393 백준
    - BufferedRead

--- codingTest/src/codingTest/Main.java
+++ codingTest/src/codingTest/Main.java
@@ -1,19 +1,24 @@
 package codingTest;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(st.nextToken());
+		int result = 0;
 		
-	
+		for(int i = 1; i <= n; i++) {
+			result += i;
+		}
+
+		System.out.println(result);
 	}
 }
