c97257e247a46dd778df1810a4ea78330e33fc36
2theMoon <dlaguddh1@gmail.com>
2021-11-13 14:47:16 +0900

    버퍼리드 숙지

--- codingTest/src/codingTest/Main.java
+++ codingTest/src/codingTest/Main.java
@@ -3,6 +3,7 @@ package codingTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,13 +13,14 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
 		int n = Integer.parseInt(st.nextToken());
 		int result = 0;
 		
-		for(int i = 1; i <= n; i++) {
+		for(int i = 0; i <= n; i++) {
 			result += i;
 		}
-
 		System.out.println(result);
+		
 	}
 }
