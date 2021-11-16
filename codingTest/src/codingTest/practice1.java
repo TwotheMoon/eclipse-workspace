18cf18d366fb52053c8eaa2aaaf3ba43b8c7aad1
2theMoon <dlaguddh1@gmail.com>
2021-11-16 12:41:22 +0900

    기본 코딩 복습

--- codingTest/src/codingTest/practice1.java
+++ codingTest/src/codingTest/practice1.java
@@ -1,33 +1,35 @@
 package codingTest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class practice1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException  {
 
-		// N 까지의 수 합
-		int result = 0;
-		int n = 100;
-//		for(int i = 1; i <= n; i++) {
-//			result += i;
-//		}
-//		
-//		result = n * (n + 1) / 2;
-//		System.out.println(result);
-//		
-//		
-//		
-//		// for문 구구단
-//		for(int i = 1; i <= 9; i++) {
-//			System.out.println("<" + i + "단 >");
-//			
-//			for(int j = 1; j <= 9; j++) {
-//				System.out.println(i + "X" + j + "=" + i * j );
-//			}
-//		} 
-		
-		
-	  
-		
+	
+	for(int i = 2; i <= 9; i++) {
+		System.out.println(i + "단");
+		for(int j = 1; j <= 9; j++) {
+			System.out.println(i + "X" + j + "=" + i * j);
+		}
 	}
 	
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	
+	int n = Integer.parseInt(st.nextToken());
+	int result = 0;
+	
+	for(int i = 0; i <= n; i++) {
+		result += i;
+	}
+	
+	System.out.println(result);
+	
+	int result2 = n * (n + 1) / 2;
+	System.out.println(result2);
+	}
 }
