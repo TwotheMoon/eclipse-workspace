941dc059f269f0bbceb2a7343e9f3dd1cb4d8503
2theMoon <dlaguddh1@gmail.com>
2021-11-17 11:51:20 +0900

    오늘 복습

--- codingTest/src/codingTest/practice1.java
+++ codingTest/src/codingTest/practice1.java
@@ -10,26 +10,12 @@ public class practice1 {
 	public static void main(String[] args) throws IOException  {
 
 	
-	for(int i = 2; i <= 9; i++) {
-		System.out.println(i + "단");
+	for(int i = 1; i <= 9; i++) {
+		System.out.println( i + "단");
 		for(int j = 1; j <= 9; j++) {
 			System.out.println(i + "X" + j + "=" + i * j);
+			}
 		}
-	}
 	
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-	
-	int n = Integer.parseInt(st.nextToken());
-	int result = 0;
-	
-	for(int i = 0; i <= n; i++) {
-		result += i;
-	}
-	
-	System.out.println(result);
-	
-	int result2 = n * (n + 1) / 2;
-	System.out.println(result2);
 	}
 }
