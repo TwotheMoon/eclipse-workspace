fbe9d5a9c4881aabe4e8620e1cbb51382aec3371
2theMoon <dlaguddh1@gmail.com>
2021-11-17 15:59:21 +0900

    백준11021

--- codingTest/src/codingTest/Main.java
+++ codingTest/src/codingTest/Main.java
@@ -1,19 +1,31 @@
 package codingTest;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int n = sc.nextInt();
+		int t = Integer.parseInt(br.readLine());
 		
-		for(int i = 1; i <= 9; i++) {
-			System.out.println(n + "X" + i + "=" + i * n);
+		StringTokenizer st;
+		for(int i = 1; i <= t; i++) {
+			
+			st = new StringTokenizer(br.readLine(), " ");
+			bw.write("Case #" + i + ": ");
+			bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n" );
 		}
+		br.close();
+		bw.flush();
+		bw.close();
 		
 	}
 }
